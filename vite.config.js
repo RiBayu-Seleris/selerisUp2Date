ba0b382f7d1ef9b67ab976b9d0f279b4fd96f740
RiBayu-Seleris <bayu@seleris.id>
2025-08-18 18:15:27 +0700

    update banner product

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
   server: {
-    allowedHosts: ["42cd6d638415.ngrok-free.app"],
+    allowedHosts: ["780bd99b21dd.ngrok-free.app"],
   },
   plugins: [
     vue(),
