98ca461fab3876c555e235ff074fdd58dae6fae3
RiBayu-Seleris <bayu@seleris.id>
2025-10-23 03:02:06 +0700

    added dist

--- src/Data/ProductList.js
+++
@@ -1,8 +0,0 @@
-// src/data/productList.js
-export const productList = [
-  { url: "/product/credit-cover", name: "Seleris Credit Cover" },
-  { url: "/product/lifins", name: "Seleris Lifins" },
-  { url: "/product/medins", name: "Seleris Medins" },
-  { url: "/product/health-care", name: "Seleris Care" },
-  { url: "/", name: "Seleris Cough" },
-];
--- src/components/Helper/slugUtil.js
+++
@@ -1,8 +0,0 @@
-export function generateSlug(text) {
-  return text
-    .toLowerCase()
-    .trim()
-    .replace(/[^a-z0-9\s-]/g, "")
-    .replace(/\s+/g, "-")
-    .replace(/-+/g, "-");
-}
