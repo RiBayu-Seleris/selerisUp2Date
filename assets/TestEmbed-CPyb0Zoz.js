5d0865ad24b0af73aa74408430837c41b1de371b
Rian Bayu Seleris <bayu@seleris.id>
2026-02-24 22:23:38 +0700

    added link to sca studio on sca product page lp

--- assets/TestEmbed-CPyb0Zoz.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-BYGlfq0K.js";import{Q as r,G as t}from"./vendor-BJDaBvDq.js";import"./charts-CWP_fP6m.js";import"./swiper-BXFUHgBo.js";const o={};function c(n,s){return t(),r("div")}const i=e(o,[["render",c]]);export{i as default};
