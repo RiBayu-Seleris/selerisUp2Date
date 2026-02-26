414963e5650796b8adc2797aa61342305012864b
Rian Bayu Seleris <bayu@seleris.id>
2026-02-26 14:07:15 +0700

    update wording and new tab for navbar menu

---
+++ assets/TestEmbed-Z7rBHusy.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-Crwc6KI_.js";import{H as r,G as t}from"./vendor-CtNZiQrC.js";import"./charts-DwMXkf7Z.js";import"./swiper-CISBSGw7.js";const o={};function c(n,s){return t(),r("div")}const i=e(o,[["render",c]]);export{i as default};
