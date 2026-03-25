b6758f0ea161c5d99a8779f606353f07295ab69c
Rian Bayu Seleris <bayu@seleris.id>
2026-03-25 13:38:35 +0700

    update company page

--- src/components/About/Herobackup.vue
+++ src/components/About/Herobackup.vue
@@ -37,10 +37,10 @@ import FrameCaptionAboutHero from "@/assets/images/About-frame-hero2.svg";
         </div>
       </div>
       <div
-        class="relative flex flex-col gap-y-5 md:gap-y-0 w-full h-auto md:w-full lg:w-[60%] xl:w-auto justify-end lg:justify-end items-end pt-10 md:pt-5 lg:pt-0"
+        class="relative flex flex-col w-full h-auto md:w-full lg:w-[60%] xl:w-auto justify-end lg:justify-end items-end pt-5 md:pt-5 lg:pt-0"
       >
         <div
-          class="hidden md:flex absolute md:w-[80%] xl:w-[75%] h-auto md:-top-[10px] xl:-top-[24px] md:-left-[25%] xl:-left-[25%]"
+          class="absolute w-[60%] md:w-[80%] xl:w-[75%] h-auto top-20 left-[3%] md:-top-[10px] xl:-top-[24px] md:-left-[25%] xl:-left-[25%]"
         >
           <div class="relative w-full h-full">
             <img
@@ -56,7 +56,7 @@ import FrameCaptionAboutHero from "@/assets/images/About-frame-hero2.svg";
             >
               <div class="w-auto h-auto flex justify-center items-center">
                 <div
-                  class="relative md:w-5 md:h-5 xl:w-6 xl:h-6 flex justify-center items-center"
+                  class="relative w-5 h-5 md:w-5 md:h-5 xl:w-6 xl:h-6 flex justify-center items-center"
                 >
                   <!-- ping effect -->
                   <div
@@ -64,10 +64,10 @@ import FrameCaptionAboutHero from "@/assets/images/About-frame-hero2.svg";
                   ></div>
                   <!-- circle asli -->
                   <div
-                    class="md:w-5 md:h-5 xl:w-6 xl:h-6 bg-[#2AB857] rounded-full md:p-[2px] xl:p-[2.5px] flex justify-center items-center"
+                    class="w-5 h-5 md:w-5 md:h-5 xl:w-6 xl:h-6 bg-[#2AB857] rounded-full p-[2px] xl:p-[2.5px] flex justify-center items-center"
                   >
                     <div
-                      class="w-full h-full bg-white rounded-full md:p-[3.5px] xl:p-[5px] flex justify-center items-center"
+                      class="w-full h-full bg-white rounded-full p-[3.5px] xl:p-[5px] flex justify-center items-center"
                     >
                       <div
                         class="w-full h-full bg-[#2AB857] rounded-full"
@@ -79,7 +79,7 @@ import FrameCaptionAboutHero from "@/assets/images/About-frame-hero2.svg";
               <div class="w-full h-auto flex flex-col justify-center gap-y-1.5">
                 <div class="w-auto h-auto">
                   <p
-                    class="text-[#195279] font-[400] text-[14px] xl:text-[16px]"
+                    class="text-[#195279] font-[400] text-[12px] md:text-[14px] xl:text-[16px]"
                   >
                     Seleris Development Office
                   </p>
@@ -98,48 +98,8 @@ import FrameCaptionAboutHero from "@/assets/images/About-frame-hero2.svg";
         <img
           :src="AboutHero"
           alt="About Banner"
-          class="w-full h-auto md:h-[380px] lg:h-[400px] xl:h-[500px] object-contain md:object-right px-8 md:px-0"
+          class="w-full h-[75%] md:h-[380px] lg:h-[400px] xl:h-[500px] object-contain object-right sm:pr-0"
         />
-        <!-- MOBILE -->
-        <div class="absolute flex md:hidden w-full h-auto px-12 bottom-4">
-          <a
-            href="https://maps.app.goo.gl/ywhqmCdfhGod9Mps6"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="relative w-full h-full flex flex-row px-4 py-3 gap-x-5 bg-white rounded-2xl drop-shadow-[0_10px_15px_rgba(0,0,0,0.05)]"
-          >
-            <div class="w-auto h-auto flex justify-center items-center">
-              <div class="relative w-5 h-5 flex justify-center items-center">
-                <!-- ping effect -->
-                <div
-                  class="absolute w-full h-full bg-[#2AB857] rounded-full animate-ping opacity-75"
-                ></div>
-                <!-- circle asli -->
-                <div
-                  class="w-5 h-5 bg-[#2AB857] rounded-full p-[2px] flex justify-center items-center"
-                >
-                  <div
-                    class="w-full h-full bg-white rounded-full p-[3.5px] flex justify-center items-center"
-                  >
-                    <div class="w-full h-full bg-[#2AB857] rounded-full"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="w-full h-auto flex flex-col justify-center gap-y-1.5">
-              <div class="w-auto h-auto">
-                <p class="text-[#195279] font-[400] text-[16px]">
-                  Seleris Development Office
-                </p>
-              </div>
-              <div class="w-auto h-auto">
-                <p class="text-[#8E98A8] font-[300] text-[12px]">
-                  Royal Spring , Jakarta Selatan.....
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
       </div>
     </div>
   </div>
